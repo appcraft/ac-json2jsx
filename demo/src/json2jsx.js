94fbd98192ebe915aa70cf5fbbd978641de27eef
Gregory Potdevin <gregory.potdevin@gmail.com>
2021-11-24 22:09:12 +0100

    ADD: optimisations

--- demo/src/json2jsx.js
+++ demo/src/json2jsx.js
@@ -1,145 +1,201 @@
-import React from 'react'
+import React from "react";
 
 function unop(op, arg) {
   switch (op) {
-    case "+": return +arg
-    case "-": return -arg
-    case "!": return !arg
-    case "~": return ~arg
+    case "+":
+      return +arg;
+    case "-":
+      return -arg;
+    case "!":
+      return !arg;
+    case "~":
+      return ~arg;
     default: {
-      console.error("unhandled unop", op)
-      return null
+      console.error("unhandled unop", op);
+      return null;
     }
   }
 }
 
 function binop(op, left, right) {
   switch (op) {
-    case '+': return left + right
-    case '-': return left - right
-    case '*': return left * right
-    case '/': return left / right
-    case '%': return left % right
-    case '==': return left == right
-    case '!=': return left != right
-    case '===': return left === right
-    case '!==': return left !== right
-    case '>': return left > right
-    case '>=': return left >= right
-    case '<': return left < right
-    case '<=': return left <= right
-    case '&': return left & right
-    case '|': return left | right
-    case '^': return left ^ right
-    case '<<': return left << right
-    case '>>': return left >> right
-    case '>>>': return left >>> right
+    case "+":
+      return left + right;
+    case "-":
+      return left - right;
+    case "*":
+      return left * right;
+    case "/":
+      return left / right;
+    case "%":
+      return left % right;
+    case "==":
+      return left == right;
+    case "!=":
+      return left != right;
+    case "===":
+      return left === right;
+    case "!==":
+      return left !== right;
+    case ">":
+      return left > right;
+    case ">=":
+      return left >= right;
+    case "<":
+      return left < right;
+    case "<=":
+      return left <= right;
+    case "&":
+      return left & right;
+    case "|":
+      return left | right;
+    case "^":
+      return left ^ right;
+    case "<<":
+      return left << right;
+    case ">>":
+      return left >> right;
+    case ">>>":
+      return left >>> right;
     default: {
-      console.error("unhandled binop", op)
-      return null
+      console.error("unhandled binop", op);
+      return null;
     }
   }
 }
 
 function jsxEval(node, components, props) {
   if (!node || !node.type) return null;
-
   switch (node.type) {
-    case "Identifier": return props[node.name];
-    case "Value": return node.value;
+    case "Identifier":
+      return props[node.name];
+    case "Value":
+      return node.value;
     case "TemplateLiteral": {
-      let str = ""
-      for (let i=0; i<node.quasis.length; i++) {
-        str += node.quasis[i]
-        if (i < node.expressions.length){
-          str += jsxEval(node.expressions[i], components, props)
+      let str = "";
+      for (let i = 0; i < node.quasis.length; i++) {
+        str += node.quasis[i];
+        if (i < node.expressions.length) {
+          str += jsxEval(node.expressions[i], components, props);
         }
       }
-      return str
+      return str;
     }
     case "UnaryExpression": {
-      return unop(node.operator, jsxEval(node.argument, components, props))
+      return unop(node.operator, jsxEval(node.argument, components, props));
     }
     case "BinaryExpression": {
       return binop(
-        node.operator, 
+        node.operator,
         jsxEval(node.left, components, props),
         jsxEval(node.right, components, props)
-      )
+      );
     }
     case "LogicalExpression": {
-      const left = jsxEval(node.left, components, props)
+      const left = jsxEval(node.left, components, props);
       switch (node.operator) {
         case "&&": {
-          if (!left) return false // Slight semantic change for React Native (simplified {str && <Text>{str}</Text>})
-          return jsxEval(node.right, components, props)
+          if (!left) return false; // Slight semantic change for React Native (simplified {str && <Text>{str}</Text>})
+          return jsxEval(node.right, components, props);
         }
         case "||": {
-          if (left) return left
-          return jsxEval(node.right, components, props)
+          if (left) return left;
+          return jsxEval(node.right, components, props);
         }
         default: {
-          console.error("unhandled logicop", node.operator)
-          return null
+          console.error("unhandled logicop", node.operator);
+          return null;
         }
       }
     }
-    case "ConditionalExpression": { 
-      const res = jsxEval(node.test, components, props)
+    case "ConditionalExpression": {
+      const res = jsxEval(node.test, components, props);
       if (res) {
-        return jsxEval(node.consequent, components, props)
+        return jsxEval(node.consequent, components, props);
       }
-      return jsxEval(node.alternate, components, props)
+      return jsxEval(node.alternate, components, props);
     }
     case "MemberExpression": {
-      const { object, property } = node
-      const obj = jsxEval(object, components, props)
-      if (!obj) return null
+      const { object, property } = node;
+      const obj = jsxEval(object, components, props);
+      if (!obj) return null;
       if (property.type === "Identifier") {
-        const res = obj[property.name]
+        const res = obj[property.name];
         if (typeof res === "function") {
-          res._this = obj
+          res._this = obj;
         }
         return res;
       }
       return null;
     }
     case "ObjectExpression": {
-      const result = {}
+      const result = {};
       for (let key in node.properties) {
-        result[key] = jsxEval(node.properties[key], components, props)
+        result[key] = jsxEval(node.properties[key], components, props);
       }
-      return result
+      return result;
     }
     case "ArrayExpression": {
-      return node.elements.map(n => jsxEval(n, components, props))
+      return node.elements.map((n) => jsxEval(n, components, props));
     }
     case "CallExpression": {
-      const callee = jsxEval(node.callee, components, props)
+      const callee = jsxEval(node.callee, components, props);
       // console.log("callee", callee, callee && callee._this)
-      if (!callee) return null
-      return callee.apply(callee._this, node.arguments.map(arg => jsxEval(arg, components, props)))
+      if (!callee) return null;
+      return callee.apply(
+        callee._this,
+        node.arguments.map((arg) => jsxEval(arg, components, props))
+      );
     }
     case "ArrowFunctionExpression": {
-      const { body, params } = node
-      return (...args) => jsxEval(body, components, {...props, ...params.reduce((acc, param, i) => ({...acc, [param.name]: args[i]}), {})} )
+      const { body, params } = node;
+      return (...args) =>
+        jsxEval(body, components, {
+          ...props,
+          ...params.reduce(
+            (acc, param, i) => ({ ...acc, [param.name]: args[i] }),
+            {}
+          ),
+        });
     }
     case "Component": {
-      const elementProps = {}
-      for (let prop of node.props) {
-        elementProps[prop.name] = prop.value ? jsxEval(prop.value, components, props) : true
+      let elementProps = {};
+
+      if (
+        node.props.length === 1 &&
+        node.props[0].type === "JSXAttributeValues"
+      ) {
+        // Only prop with all the values in it
+        elementProps = node.props[0].values;
+      } else {
+        for (const prop of node.props) {
+          if (prop.type === "JSXAttributeValues") {
+            Object.assign(elementProps, prop.values);
+          } else if (prop.type === "JSXAttributeValue") {
+            elementProps[prop.name] = prop.value;
+          } else {
+            elementProps[prop.name] = prop.value
+              ? jsxEval(prop.value, components, props)
+              : true;
+          }
+        }
       }
-      const children = node.children.map(c => jsxEval(c, components, props));
+
+      const children = node.children.map((c) => jsxEval(c, components, props));
       // console.log("Create", node.component, elementProps, children)
-      return React.createElement(components[node.component] || node.component, elementProps, ...children);
+      return React.createElement(
+        components[node.component] || node.component,
+        elementProps,
+        ...children
+      );
     }
     default: {
-      console.warn("Unknown node type", node.type, node)
-      return null
+      console.warn("Unknown node type", node.type, node);
+      return null;
     }
   }
 }
 
-const json2jsx = jsxEval
+const json2jsx = jsxEval;
 
-export default json2jsx
+export default json2jsx;
--- demo/src/jsx2json.js
+++ demo/src/jsx2json.js
@@ -1,128 +1,215 @@
-import { transform } from 'babel-standalone'
+import { transform } from "babel-standalone";
 
-
-function isEmptyString(node){
-  return (node.type === "StringLiteral" || node.type === "Value") && /^([ ]|\n)*\n([ ]|\n)*$/.test(node.value)
+function isEmptyString(node) {
+  return (
+    (node.type === "StringLiteral" || node.type === "Value") &&
+    /^([ ]|\n)*\n([ ]|\n)*$/.test(node.value)
+  );
 }
 
-function removeEmptyChildren(children){
-  if (!children) return children
-  return children.filter(c => !isEmptyString(c))
+function removeEmptyChildren(children) {
+  if (!children) return children;
+  return children.filter((c) => !isEmptyString(c));
 }
 
-function propertiesObject(properties){
-  const obj = {}
-  for(let prop of properties){
-    obj[prop.key] = prop.value
+function propertiesObject(properties) {
+  const obj = {};
+  for (let prop of properties) {
+    obj[prop.key] = prop.value;
   }
-  return obj
+  return obj;
 }
 
-function partialEval(node){
-  switch(node.type){
-    case "Value": return node
+function partialEval(node) {
+  switch (node.type) {
+    case "Value":
+      return node;
     case "ObjectExpression": {
-      let isConstant = true
-      const value = {}
-      for(let prop in node.properties){
-        node.properties[prop] = partialEval(node.properties[prop])
-        if (node.properties[prop].type === "Value"){
-          value[prop] = node.properties[prop].value
+      let isConstant = true;
+      const value = {};
+      for (let prop in node.properties) {
+        node.properties[prop] = partialEval(node.properties[prop]);
+        if (node.properties[prop].type === "Value") {
+          value[prop] = node.properties[prop].value;
         } else {
-          isConstant = false
-          break
+          isConstant = false;
+          break;
         }
       }
-      if (!isConstant) return node
+      if (!isConstant) return node;
       return {
         type: "Value",
-        value: value
-      }
+        value: value,
+      };
     }
     case "ArrayExpression": {
-      let isConstant = true
-      for(let element of node.elements){
+      let isConstant = true;
+      for (let element of node.elements) {
         if (element.type !== "Value") {
-          isConstant = false
-          break
+          isConstant = false;
+          break;
         }
       }
-      if (!isConstant){
-        return node
+      if (!isConstant) {
+        return node;
       }
       return {
         type: "Value",
-        value: node.elements.map(n => n.value)
-      }
+        value: node.elements.map((n) => n.value),
+      };
     }
-    default: return node
+    default:
+      return node;
   }
 }
 
+function compactProps(props) {
+  if (props.length === 0) return [];
+  return props.reduce((array, cur) => {
+    if (array.length === 0 || cur.type !== "JSXAttributeValue") {
+      // Can't compact
+      array.push(cur);
+      return array;
+    }
+    const len = array.length;
+    const prev = array[len - 1];
+    if (prev.type === "JSXAttributeValue") {
+      array[len - 1] = {
+        type: "JSXAttributeValues",
+        values: { [prev.name]: prev.value, [cur.name]: cur.value },
+      };
+    } else if (prev.type === "JSXAttributeValues") {
+      array[len - 1].values[cur.name] = cur.value;
+    } else {
+      array.push(cur);
+    }
+    return array;
+  }, []);
+}
 
-function clean(node){
-  if (!node) return node
+function clean(node) {
+  if (!node) return node;
 
-  switch(node.type){
-    case "File": return clean(node.program)
-    case "Program": return clean(node.body[0])
-    case "ExpressionStatement": return clean(node.expression)
-    case "Identifier": return { type: node.type, name: node.name }
-    case "StringLiteral": 
-    case "NumericLiteral": 
-    case "BooleanLiteral": 
-    case "JSXText": return { type: "Value", value: node.value }
-    case "TemplateLiteral": return { type: node.type, expressions: node.expressions.map(clean), quasis: node.quasis.map(q => q.value.raw) }
-    case "CallExpression": return { type: node.type, callee: clean(node.callee), arguments: node.arguments.map(clean)}
-    case "MemberExpression": return { type: node.type, object: clean(node.object), property: clean(node.property)}
-    case "UnaryExpression": return { type: node.type, operator: node.operator, argument: clean(node.argument)}
+  switch (node.type) {
+    case "File":
+      return clean(node.program);
+    case "Program":
+      return clean(node.body[0]);
+    case "ExpressionStatement":
+      return clean(node.expression);
+    case "Identifier":
+      return { type: node.type, name: node.name };
+    case "StringLiteral":
+    case "NumericLiteral":
+    case "BooleanLiteral":
+    case "JSXText":
+      return { type: "Value", value: node.value };
+    case "TemplateLiteral":
+      return {
+        type: node.type,
+        expressions: node.expressions.map(clean),
+        quasis: node.quasis.map((q) => q.value.raw),
+      };
+    case "CallExpression":
+      return {
+        type: node.type,
+        callee: clean(node.callee),
+        arguments: node.arguments.map(clean),
+      };
+    case "MemberExpression":
+      return {
+        type: node.type,
+        object: clean(node.object),
+        property: clean(node.property),
+      };
+    case "UnaryExpression":
+      return {
+        type: node.type,
+        operator: node.operator,
+        argument: clean(node.argument),
+      };
     case "BinaryExpression":
-    case "LogicalExpression": return { type: node.type, left: clean(node.left), operator: node.operator, right: clean(node.right)}
-    case "ConditionalExpression": return { 
-      type: node.type,
-      test: clean(node.test),
-      consequent: clean(node.consequent),
-      alternate: clean(node.alternate)
-    }
+    case "LogicalExpression":
+      return {
+        type: node.type,
+        left: clean(node.left),
+        operator: node.operator,
+        right: clean(node.right),
+      };
+    case "ConditionalExpression":
+      return {
+        type: node.type,
+        test: clean(node.test),
+        consequent: clean(node.consequent),
+        alternate: clean(node.alternate),
+      };
     // case "ObjectExpression": return { type: node.type, properties: node.properties.map(clean)}
-    case "ObjectExpression": return partialEval({ type: node.type, properties: propertiesObject(node.properties.map(clean))})
-    case "ArrayExpression": return partialEval({ type: node.type, elements: node.elements.map(clean)})
-    case "ArrowFunctionExpression": return { type: node.type, params: node.params.map(clean), body: clean(node.body) }
-    case "ObjectProperty": return { type: node.type, key: node.key.name, value: clean(node.value)}
-    case "JSXExpressionContainer": return clean(node.expression)
-    case "JSXElement": return {
-      type: 'Component',
-      component: node.openingElement.name.name,
-      props: node.openingElement.attributes.map(clean),
-      children: node.children && removeEmptyChildren(node.children.map(clean))
-    }
-    case "JSXAttribute": return {
-      type: "JSXAttribute",
-      name: node.name.name,
-      value: clean(node.value)
+    case "ObjectExpression":
+      return partialEval({
+        type: node.type,
+        properties: propertiesObject(node.properties.map(clean)),
+      });
+    case "ArrayExpression":
+      return partialEval({
+        type: node.type,
+        elements: node.elements.map(clean),
+      });
+    case "ArrowFunctionExpression":
+      return {
+        type: node.type,
+        params: node.params.map(clean),
+        body: clean(node.body),
+      };
+    case "ObjectProperty":
+      return { type: node.type, key: node.key.name, value: clean(node.value) };
+    case "JSXExpressionContainer":
+      return clean(node.expression);
+    case "JSXElement":
+      return {
+        type: "Component",
+        component: node.openingElement.name.name,
+        props: compactProps(node.openingElement.attributes.map(clean)),
+        children:
+          node.children && removeEmptyChildren(node.children.map(clean)),
+      };
+    case "JSXAttribute": {
+      const value = clean(node.value);
+      if (value?.type === "Value") {
+        // Slightly more compact version
+        return {
+          type: "JSXAttributeValue",
+          name: node.name.name,
+          value: value.value,
+        };
+      }
+      return {
+        type: "JSXAttribute",
+        name: node.name.name,
+        value: value,
+      };
     }
     default: {
-      console.warn("Unhandled node type", node.type, node) 
-      return {...node}
-    } 
+      console.warn("Unhandled node type", node.type, node);
+      return { ...node };
+    }
   }
 }
 
-export default function(jsx){
+export default function (jsx) {
   try {
-    const result = transform(jsx, { 
+    const result = transform(jsx, {
       // presets: ["es2015", "react", "stage-1"],
-      plugins: [ [ require('babel-plugin-syntax-jsx') ] ]
+      plugins: [[require("babel-plugin-syntax-jsx")]],
       // plugins: [ [ jsxTransform ] ]
-    })
-    console.log("ast", result.ast)
+    });
+    console.log("ast", result.ast);
     return {
-      ast: clean(result.ast)
-    }
-  } catch(e){
-    console.log("error", e)
+      ast: clean(result.ast),
+    };
+  } catch (e) {
+    console.log("error", e);
     return {
-      error: e.message
-    }
+      error: e.message,
+    };
   }
 }
